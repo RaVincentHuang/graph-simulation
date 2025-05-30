7f8c15f3d194471859b271224326e350bb71ba41
RaVincentHuang <18573104293@163.com>
2025-05-30 11:36:51 +0800

    [Change|change the hyper simulation interface]

--- src/algorithm/hyper_simulation.rs
+++ src/algorithm/hyper_simulation.rs
@@ -2,10 +2,13 @@ use std::collections::{HashMap, HashSet};
 
 use graph_base::interfaces::{edge::DirectedHyperedge, graph::SingleId, hypergraph::{ContainedDirectedHyperedge, ContainedHyperedge, DirectedHypergraph, Hypergraph}, typed::{Type, Typed}};
 
-pub trait LMatch<'a>: Hypergraph<'a> {
+pub trait LMatch {
+    type Edge;
     // fn l_match(&'a self, e: &'a Self::Edge, e_prime: &'a Self::Edge) -> HashMap<&'a Self::Node, &'a HashSet<&'a Self::Node>>;
+    fn new() -> Self;
+    fn l_match_with_node_mut(&mut self, e: &Self::Edge, e_prime: &Self::Edge, u: usize) -> &HashSet<usize>;
     fn l_match_with_node(&self, e: &Self::Edge, e_prime: &Self::Edge, u: usize) -> &HashSet<usize>;
-    fn dom(&self, e: &Self::Edge, e_prime: &Self::Edge) -> Vec<usize>;
+    fn dom(&self, e: &Self::Edge, e_prime: &Self::Edge) -> impl Iterator<Item = &usize>;
 }
 
 pub trait LPredicate<'a>: Hypergraph<'a> {
@@ -14,27 +17,24 @@ pub trait LPredicate<'a>: Hypergraph<'a> {
     fn l_predicate_set(&'a self, x: &HashSet<&'a Self::Node>, y: &HashSet<&'a Self::Node>) -> bool;
 }
 
-pub trait HyperSimulation<'a> {
-    type Node;
+pub trait HyperSimulation<'a>: Hypergraph<'a> {
 
-    fn get_simulation_fixpoint(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
-    fn get_simulation_recursive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
-    fn get_simulation_naive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
+    fn get_simulation_fixpoint(&'a self, other: &'a Self, l_match: &mut impl LMatch<Edge = Self::Edge>) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
+    fn get_simulation_recursive(&'a self, other: &'a Self, l_match: &mut impl LMatch<Edge = Self::Edge>) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
+    fn get_simulation_naive(&'a self, other: &'a Self, l_match: &mut impl LMatch<Edge = Self::Edge>) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
 }
 
 impl<'a, H> HyperSimulation<'a> for H 
-where H: Hypergraph<'a> + Typed<'a> + LMatch<'a> + LPredicate<'a> + ContainedHyperedge<'a> {
-    type Node = H::Node;
-
-    fn get_simulation_fixpoint(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
+where H: Hypergraph<'a> + Typed<'a> + LPredicate<'a> + ContainedHyperedge<'a> {
+    fn get_simulation_fixpoint(&'a self, other: &'a Self, l_match: &mut impl LMatch<Edge = Self::Edge>) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
         todo!()
     }
 
-    fn get_simulation_recursive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
+    fn get_simulation_recursive(&'a self, other: &'a Self, l_match: &mut impl LMatch<Edge = Self::Edge>) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
         todo!()
     }
 
-    fn get_simulation_naive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
+    fn get_simulation_naive(&'a self, other: &'a Self, l_match: &mut impl LMatch<Edge = Self::Edge>) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
         let self_contained_hyperedge = self.get_hyperedges_list();
         let other_contained_hyperedge = other.get_hyperedges_list();
 
@@ -49,7 +49,7 @@ where H: Hypergraph<'a> + Typed<'a> + LMatch<'a> + LPredicate<'a> + ContainedHyp
                         for e_prime in other.contained_hyperedges(&other_contained_hyperedge, v) {
                             if self.l_predicate_edge(e, e_prime) {
                                 // let l_match = self.l_match(e, e_prime);
-                                let id_set = self.l_match_with_node(e, e_prime, u.id());
+                                let id_set = l_match.l_match_with_node_mut(e, e_prime, u.id());
                                 l_match_union = l_match_union.union(&id_set).copied().collect();
                             }
                         }
@@ -81,8 +81,8 @@ where H: Hypergraph<'a> + Typed<'a> + LMatch<'a> + LPredicate<'a> + ContainedHyp
                         }
                         for e_prime in other.contained_hyperedges(&other_contained_hyperedge, v) {
                             if self.l_predicate_edge(e, e_prime) {
-                                if self.dom(e, e_prime).iter().all(|u_prime| {
-                                    self.l_match_with_node(e, e_prime, *u_prime).iter().map(|id| {other.get_node_by_id(*id)}).any(|v_prime| {
+                                if l_match.dom(e, e_prime).all(|u_prime| {
+                                    l_match.l_match_with_node(e, e_prime, u_prime.clone()).iter().map(|id| {other.get_node_by_id(*id)}).any(|v_prime| {
                                         if let Some(v_prime) = v_prime {
                                             return simulation.get(u).unwrap().contains(v_prime);
                                         } else {
@@ -111,74 +111,74 @@ where H: Hypergraph<'a> + Typed<'a> + LMatch<'a> + LPredicate<'a> + ContainedHyp
     }
 }
 
-pub trait DiHyperSimulation<'a> {
-    type Node;
+// pub trait DiHyperSimulation<'a> {
+//     type Node;
 
-    fn get_simulation_fixpoint(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
-    fn get_simulation_recursive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
-    fn get_simulation_native(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
-}
+//     fn get_simulation_fixpoint(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
+//     fn get_simulation_recursive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
+//     fn get_simulation_native(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>>;
+// }
 
-impl<'a, H> DiHyperSimulation<'a> for H 
-where 
-    H: DirectedHypergraph<'a> + Typed<'a> + LMatch<'a> + LPredicate<'a> + ContainedDirectedHyperedge<'a>,
-    H::Node: Type, H::Edge: DirectedHyperedge {
-    type Node = H::Node;
+// impl<'a, H> DiHyperSimulation<'a> for H 
+// where 
+//     H: DirectedHypergraph<'a> + Typed<'a> + LMatch<'a> + LPredicate<'a> + ContainedDirectedHyperedge<'a>,
+//     H::Node: Type, H::Edge: DirectedHyperedge {
+//     type Node = H::Node;
 
-    fn get_simulation_fixpoint(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
-        todo!()
-    }
+//     fn get_simulation_fixpoint(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
+//         todo!()
+//     }
 
-    fn get_simulation_recursive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
-        todo!()
-    }
+//     fn get_simulation_recursive(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
+//         todo!()
+//     }
 
-    fn get_simulation_native(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
-        // let mut simulation: HashMap<&'a Self::Node, HashSet<&'a Self::Node>> = self.nodes().map(|u| {
-        //     let res = other.nodes().filter(|v| {
-        //         self.type_same(u, *v)
-        //     }).collect();
-        //     (u, res)
-        // }).collect();
+//     fn get_simulation_native(&'a self, other: &'a Self) -> HashMap<&'a Self::Node, HashSet<&'a Self::Node>> {
+//         // let mut simulation: HashMap<&'a Self::Node, HashSet<&'a Self::Node>> = self.nodes().map(|u| {
+//         //     let res = other.nodes().filter(|v| {
+//         //         self.type_same(u, *v)
+//         //     }).collect();
+//         //     (u, res)
+//         // }).collect();
 
-        // let self_contained_hyperedge = self.get_hyperedges_src();
-        // let other_contained_hyperedge = other.get_hyperedges_src();
+//         // let self_contained_hyperedge = self.get_hyperedges_src();
+//         // let other_contained_hyperedge = other.get_hyperedges_src();
 
-        // let mut changed = false;
-        // while !changed {
-        //     for u in self.nodes() {
-        //         let mut need_delete = Vec::new();
-        //         for v in simulation.get(u).unwrap() {
-        //             let mut _delete = true;
-        //             for e in self.contained_hyperedges(&self_contained_hyperedge, u) {
-        //                 if !_delete {
-        //                     break;
-        //                 }
-        //                 for e_prime in other.contained_hyperedges(&other_contained_hyperedge, v) {
-        //                     if self.l_predicate_edge(e, e_prime) {
-        //                         let l_match = self.l_match(e, e_prime);
-        //                         if l_match(u).id() == v.id() 
-        //                             && self.dom(l_match).all(|u_prime| {
-        //                                 simulation.get(u).unwrap().contains(&u_prime)
-        //                         }) {
-        //                             _delete = false;
-        //                             break;
-        //                         }
-        //                     }
-        //                 }
-        //             }
-        //             if _delete {
-        //                 need_delete.push(v.clone());
-        //             }
-        //         }
-        //         for v in need_delete {
-        //             simulation.get_mut(u).unwrap().remove(v);
-        //             changed = true;
-        //         }
-        //     }
-        // }
+//         // let mut changed = false;
+//         // while !changed {
+//         //     for u in self.nodes() {
+//         //         let mut need_delete = Vec::new();
+//         //         for v in simulation.get(u).unwrap() {
+//         //             let mut _delete = true;
+//         //             for e in self.contained_hyperedges(&self_contained_hyperedge, u) {
+//         //                 if !_delete {
+//         //                     break;
+//         //                 }
+//         //                 for e_prime in other.contained_hyperedges(&other_contained_hyperedge, v) {
+//         //                     if self.l_predicate_edge(e, e_prime) {
+//         //                         let l_match = self.l_match(e, e_prime);
+//         //                         if l_match(u).id() == v.id() 
+//         //                             && self.dom(l_match).all(|u_prime| {
+//         //                                 simulation.get(u).unwrap().contains(&u_prime)
+//         //                         }) {
+//         //                             _delete = false;
+//         //                             break;
+//         //                         }
+//         //                     }
+//         //                 }
+//         //             }
+//         //             if _delete {
+//         //                 need_delete.push(v.clone());
+//         //             }
+//         //         }
+//         //         for v in need_delete {
+//         //             simulation.get_mut(u).unwrap().remove(v);
+//         //             changed = true;
+//         //         }
+//         //     }
+//         // }
 
-        // simulation
-        todo!()
-    }
-}
+//         // simulation
+//         todo!()
+//     }
+// }
